9977959fc526815b5184039ea5832acc3cb2d3c8
Luis Gonzalo <afthedark@hotmail.com>
2025-05-28 12:17:34 -0400

    update bat new

--- start_backend.bat
+++ start_backend.bat
@@ -1,3 +1,8 @@
 @echo off
+title Backend MChicken API
 cd /d "%~dp0"
-start /min cmd /c "node server.js"
+echo Iniciando servidor backend...
+node server.js
+echo.
+echo El servidor se ha detenido. Presiona cualquier tecla para cerrar.
+pause >nul
